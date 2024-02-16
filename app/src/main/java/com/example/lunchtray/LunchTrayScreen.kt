49148a58d40ba7fbab906897d0e471c1908382fd
KatePril <ket14pril14@gmail.com>
2024-02-16 21:09:05 +0200

    Add app bar

--- app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -17,18 +17,29 @@ package com.example.lunchtray
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Resources.Theme
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBarDefaults.containerColor
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -51,7 +62,34 @@ enum class AppScreens(@StringRes val title: Int) {
     Checkout(R.string.order_checkout)
 }
 
-// TODO: AppBar
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    currentScreen: AppScreens,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    (
+        TopAppBar(
+            title = { Text(stringResource(currentScreen.title)) },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+            modifier = modifier,
+            navigationIcon = {
+                if (canNavigateBack) {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back_button)
+                        )
+                    }
+                }
+            }
+        )
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -66,7 +104,10 @@ fun LunchTrayApp(
 
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            AppBar(currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
@@ -90,7 +131,9 @@ fun LunchTrayApp(
             composable(route = AppScreens.EntreeMenu.name) {
                 EntreeMenuScreen(
                     options = DataSource.entreeMenuItems,
-                    onCancelButtonClicked = {},
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
                     onNextButtonClicked = {
                         navController.navigate(AppScreens.SideDishMenu.name)
                     },
@@ -101,7 +144,9 @@ fun LunchTrayApp(
             composable(route = AppScreens.SideDishMenu.name) {
                 SideDishMenuScreen(
                     options = DataSource.sideDishMenuItems,
-                    onCancelButtonClicked = {},
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
                     onNextButtonClicked = {
                         navController.navigate(AppScreens.AccompanimentMenu.name)
                     },
@@ -112,7 +157,9 @@ fun LunchTrayApp(
             composable(route = AppScreens.AccompanimentMenu.name) {
                 AccompanimentMenuScreen(
                     options = DataSource.accompanimentMenuItems,
-                    onCancelButtonClicked = {},
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
                     onNextButtonClicked = {
                         navController.navigate(AppScreens.Checkout.name)
                     },
@@ -127,7 +174,9 @@ fun LunchTrayApp(
                     onNextButtonClicked = { subject: String, summary: String ->
                         shareOrder(context, subject = subject, summary = summary)
                     },
-                    onCancelButtonClicked = {},
+                    onCancelButtonClicked = {
+                        cancelOrderAndNavigateToStart(viewModel, navController)
+                    },
                     modifier = Modifier.fillMaxHeight()
                 )
             }
@@ -149,4 +198,12 @@ private fun shareOrder(context: Context, subject: String, summary: String) {
         )
     )
 
+}
+
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(AppScreens.Start.name, inclusive = false)
 }
